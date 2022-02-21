d6489732bdd5cc14fd206378c8e2fe871202f099
Greg Bizup <g.bizup@gmail.com>
2022-02-20 23:18:57 -0800

    plot conc by date, color by site

--- aq.R
+++ aq.R
@@ -1,19 +1,39 @@
 library(data.table)
 library(ggplot2)
+library(dplyr)
+
+plot <- TRUE
 
 data <- fread("data/alabama_pm.csv",
               select = c("Date",
                          "Daily Mean PM2.5 Concentration",
                          "UNITS",
-                         "Site ID"))
+                         "Site ID",
+                         "Site Name"))
 
-data <- data[, .(Date, conc = `Daily Mean PM2.5 Concentration`, site = `Site ID`)]
+data <- data[, .(Date = as.Date(Date, format = "%m/%d/%y"),
+                 conc = `Daily Mean PM2.5 Concentration`,
+                 site = as.character(`Site ID`),
+                 name = `Site Name`,
+                 units = UNITS)]
 
-days_over <- data.table(table(data[conc > 35, Date]))[order(N, -V1), .(Date = V1, count = N)]
-bad_sites <- data.table(table(data[conc > 35, site]))[order(-N), .(site = V1, days_over = N)]
+yearly_mean <- data[, .(mean_conc_ugm3 = mean(conc),
+                        attainment = mean(conc) < 13),
+                        by = .(site, name)]
 
-ggplot(days_over, aes(Date, count)) +
-    geom_col()
+daily_limit_exceeded <- data[conc > 35L, .(count = sum(table(site))), Date]
 
-ggplot(bad_sites, aes(site, days_over)) +
-    geom_col()
+ggplot(data) +
+   geom_point(aes(Date, conc, color = site)) +
+   geom_smooth(aes(Date, conc)) +
+   labs(title = "Measured concentration by date",
+        x = "Date",
+        y = "Concentration (ug/m^3)"
+   ) +
+   scale_color_discrete(name = "Site") +
+   theme(
+        plot.title = element_text(size = 30, face = "bold")
+   ) +
+   theme_bw()
+
+data
