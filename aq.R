f2cc7b3fb14e91f6880db756718725253a183031
Gregory Bizup <g.bizup@gmail.com>
2022-02-22 14:07:10 -0800

    compared dplyr and data.table

--- aq.R
+++ aq.R
@@ -3,8 +3,6 @@ library(ggplot2)
 library(ggthemes)
 library(dplyr)
 
-plot <- TRUE
-
 data <- fread("data/alabama_pm.csv",
               select = c("Date",
                          "Daily Mean PM2.5 Concentration",
@@ -18,29 +16,35 @@ data <- data[, .(Date = as.Date(Date, format = "%m/%d/%y"),
                  name = `Site Name`,
                  units = UNITS)]
 
-yearly_mean <- data[, .(mean_conc_ugm3 = mean(conc),
+table_mean <- data[, .(mean_conc_ugm3 = mean(conc),
                         attainment = mean(conc) < 13),
-                        by = .(site, name)]
+                        .(site, name = ifelse(name == "", site, name))]
 
-daily_limit_exceeded <- data[conc > 35L, .(count = sum(table(site))), Date]
+dplyr_mean <- group_by(data, site, name) %>%
+                summarize(mean_conc_ugm3 = mean(conc)) %>%
+                mutate(attainment = mean_conc_ugm3 < 13,
+                       name = ifelse(name == "", site, name))
 
-ggplot(data) +
-   geom_point(aes(Date, conc, color = site)) +
-   geom_hline(yintercept = 35, linetype = "dashed") +
-   geom_text(
-             aes(x = as.Date("2020/01/01"),
-                 y = 35, label = "35ug/m^3 daily NAAQS",
-                 hjust = 0.2,
-                 vjust = -1),
-             data = data.frame()) +
-   geom_smooth(aes(Date, conc)) +
-   labs(title = "Measured concentration by date",
-        x = "Date",
-        y = "Concentration (ug/m^3)"
-   ) +
-   scale_color_discrete(name = "Site") +
-   theme(
-        plot.title = element_text(size = 30, face = "bold")) +
-   theme_clean()
+table_mean
 
-data
+dplyr_mean
+
+#p <- ggplot(data) +
+#       geom_point(aes(Date, conc, color = site), size = 1.25) +
+#       geom_hline(yintercept = 35, linetype = "dashed") +
+#       geom_text(
+#                 aes(x = as.Date("2020/01/01"),
+#                     y = 35, label = "35ug/m^3 daily NAAQS",
+#                     hjust = 0.3,
+#                     vjust = -0.5),
+#                 data = data.frame()) +
+#       labs(title = "Measured concentration by date",
+#            x = "Date",
+#            y = "Concentration (ug/m^3)"
+#       ) +
+#       scale_color_discrete(name = "Site") +
+#       theme(
+#            plot.title = element_text(size = 30, face = "bold")) +
+#       theme_solarized(light = FALSE)
+#
+#ggsave("/mnt/c/Users/gregory.bizup/Pictures/ggplot.png", plot = p, width = 15, height = 7, dpi = 1000)
