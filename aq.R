f62b54c15a8ffd925c97e5af2037a9c766925c76
Gregory Bizup <g.bizup@gmail.com>
2022-02-24 15:54:26 -0800

    more functionalizing and bar plotting air quality data

--- aq.R
+++ aq.R
@@ -1,22 +1,54 @@
+# Functions for EPA air quality data
+
 library(ggplot2)
 library(ggthemes)
 library(dplyr)
 library(data.table)
 
-open_pmdata <- function(file) {
+open_data <- function(file) {
     data <- fread(file, select = c(1, 5, 6, 3, 8, 12))
     data <- setNames(data, c("date", "conc", "units", "site", "name", "param"))
     data <- data[, date := as.Date(date, format = "%m/%d/%y")]
     data <- data[, site := as.character(site)]
-    return(data)
+    data
 }
 
 site_yearly_mean <- function(aq_data) {
     mean <- group_by(aq_data, site, name) %>%
-            summarize(mean_conc_ugm3 = mean(conc)) %>%
-            mutate(attainment = mean_conc_ugm3 < 13,
-                name = ifelse(name == "", site, name))
-    return(mean)
+            summarize(mean_conc = mean(conc)) %>%
+            mutate(attainment = ifelse(mean_conc < 13, "Attainment", "Nonattainment"),
+                name = ifelse(name == "", site, name)) %>%
+            arrange(mean_conc)
+    mean
+}
+
+bar <- function(table, 
+                savedir = "/mnt/c/Users/gregory.bizup/Pictures/", 
+                savename = "bar", 
+                filetype = ".png") {
+    ggplot(table) +
+        geom_col(aes(x = reorder(name, mean_conc), y = mean_conc, fill = attainment)) +
+        geom_hline(yintercept = 13, linetype = "dashed") +
+        coord_flip() +
+        scale_color_discrete(name = "Attainment") +
+        labs(
+            title = "Annual mean PM2.5 conentration by site",
+            y = "Concentration (ug/m^3)",
+            x = ""
+        ) +
+        theme(
+              axis.text.x = element_text(angle = 90),
+              axis.text.y = element_text(angle = 90)
+        ) +
+        theme_solarized()
+    ggsave(paste(savedir, 
+                 savename, 
+                 filetype, 
+                 sep = ""), 
+           width = 15, 
+           height = 7, 
+           dpi = 1000)
+    print("Plot saved")
 }
 
 p <- ggplot(data) +
