2d44a51e08191aa16482282b70e1e975411787e3
Gregory Bizup <g.bizup@gmail.com>
2022-02-25 09:31:27 -0800

    minor styling edits

--- aq.R
+++ aq.R
@@ -4,20 +4,21 @@ library(ggplot2)
 library(ggthemes)
 library(dplyr)
 library(data.table)
+library(RColorBrewer)
 
 open_data <- function(file) {
     data <- fread(file, select = c(1, 5, 6, 3, 8, 12))
     data <- setNames(data, c("date", "conc", "units", "site", "name", "param"))
     data <- data[, date := as.Date(date, format = "%m/%d/%y")]
     data <- data[, site := as.character(site)]
+    data <- data[, name := ifelse(name == "", site, name)]
     data
 }
 
 site_yearly_mean <- function(aq_data) {
     mean <- group_by(aq_data, site, name) %>%
             summarize(mean_conc = mean(conc)) %>%
-            mutate(attainment = ifelse(mean_conc < 13, "Attainment", "Nonattainment"),
-                name = ifelse(name == "", site, name)) %>%
+            mutate(attainment = ifelse(mean_conc < 13, "Attainment", "Nonattainment")) %>%
             arrange(mean_conc)
     mean
 }
@@ -30,9 +31,10 @@ bar <- function(table,
         geom_col(aes(x = reorder(name, mean_conc), y = mean_conc, fill = attainment)) +
         geom_hline(yintercept = 13, linetype = "dashed") +
         coord_flip() +
-        scale_color_discrete(name = "Attainment") +
+        scale_fill_discrete(name = "Attainment", direction = -1) +
         labs(
             title = "Annual mean PM2.5 conentration by site",
+            caption = "Dashed line indicates 13ug/M^3 NAAQS",
             y = "Concentration (ug/m^3)",
             x = ""
         ) +
@@ -56,10 +58,11 @@ point <- function(table,
                 savename = "point", 
                 filetype = ".png") {
     ggplot(table) +
-        geom_point(aes(x = date, y = conc, color = site), size = 1.25) +
+        geom_point(aes(x = date, y = conc, color = name), size = 1.25) +
         geom_hline(yintercept = 35, linetype = "dashed") +
         labs(
             title = "Daily mean PM2.5 conentration by date",
+            caption = "Dashed line indicates 35ug/M^3 NAAQS",
             y = "Concentration (ug/m^3)",
             x = ""
         ) +
@@ -73,3 +76,7 @@ point <- function(table,
            dpi = 1000)
     print("Plot saved")
 }
+
+tbl <- open_data("data/alabama_pm.csv")
+ym <- site_yearly_mean(tbl)
+point(tbl, savedir = "/mnt/c/Users/gregory.bizup/Pictures/")
