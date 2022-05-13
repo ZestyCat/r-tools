fe9cca2cd53242f60368514743b72990ebe96503
Gregory Bizup <g.bizup@gmail.com>
2022-05-13 15:01:14 -0700

    messed with acdata file

---
+++ misc/add_static_or_flight_column.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+library(data.table)
+
+# Adds columns specifying which aircraft are in the static and flight databases
+
+acdata <- read_csv("~/python/python-tools/noiseplot_generator/data/acdata.csv")
+static <- read_csv("~/python/python-tools/noiseplot_generator/data/static_power_setting_list.csv")
+
+static_ac <- unique(static[["Aircraft"]]) # Get list of all static aircraft
+
+acdata <- acdata %>% 
+    mutate(flight = TRUE) %>% # All entries thus far are in flight01
+    mutate(static = is.element(aircraft, static_ac)) %>%
+    add_row(aircraft = static_ac[unlist(lapply(static_ac, function(e) !is.element(e, acdata[["aircraft"]])))],
+        flight = FALSE, static = TRUE # Find all static ac that are not elements of flight data
+    )
+
+fwrite(acdata, file = "~/python/python-tools/noiseplot_generator/data/acdata.csv")
